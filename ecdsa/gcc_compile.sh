ba584e725a9b0854a406df8ffca9280464cebdca
大大狐狸cyberfox_meow <hitfzyangdianshi@hotmail.com>
2020-11-08 06:32:48 +0800

    Update gcc_compile.sh

--- ecdsa/gcc_compile.sh
+++ ecdsa/gcc_compile.sh
@@ -1,3 +1,3 @@
 #gcc ecdsa_main.c ecdsa_f.c -L /usr/lib/x86_64-linux-gnu/ -l crypto -l ssl
 
-gcc -m32 ecdsa_main.c ecdsa_f.c -I /home/qwertyu/openssl32/include/ -L /home/qwertyu/openssl32/lib/ -l crypto -l ssl 
+gcc -m32 ecdsa_main.c ecdsa_f.c -I/home/qwertyu/openssl32/include/ -L/home/qwertyu/openssl32/lib/ -lcrypto -lssl 
