708058d1002fec7dae11b2a9bedafbd1707032aa
大大狐狸cyberfox_meow <hitfzyangdianshi@hotmail.com>
2020-12-28 11:29:50 +0800

    Update cmd_bootm.c

--- common/cmd_bootm.c
+++ common/cmd_bootm.c
@@ -235,13 +235,13 @@ int do_bootm (cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 	}//raspi_read(load_addr + k, mtd5_ADDR + k, chunk);
 	printf("%ld bytes \n", k - chunk); // size = k - chunk (excluding last chunk) 
 	sha256_csum_wd((char*)load_addr, k - chunk, sha256_sum, CHUNKSZ_SHA256);*/
-	unsigned char* p_load_addr;
+	/*unsigned char* p_load_addr;
 	p_load_addr = load_addr;
 	raspi_read(p_load_addr, mtd5_ADDR , mtd6_ADDR - mtd5_ADDR);
 
-	sha256_csum_wd(p_load_addr, mtd6_ADDR - mtd5_ADDR, sha256_sum, CHUNKSZ_SHA256);
+	/ha256_csum_wd(p_load_addr, mtd6_ADDR - mtd5_ADDR, sha256_sum, CHUNKSZ_SHA256);*/
 	printf("Current Firmware /rom (/dev/root, mtd5-mtd6) sha256 ... ");
-	//sha256_csum_wd((char*)mtd5_ADDR, mtd6_ADDR- mtd5_ADDR, sha256_sum, CHUNKSZ_SHA256);
+	sha256_csum_wd((char*)mtd5_ADDR, mtd6_ADDR- mtd5_ADDR, sha256_sum, CHUNKSZ_SHA256);
 	for (i = 0; i < 32; i++) {
 		printf("%02lx", sha256_sum[i]);
 	}
