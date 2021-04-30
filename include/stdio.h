da8f95d6bcbe15c23df10373c9dab56a13bbf08d
Aung Maw <aungmawjj12@gmail.com>
2021-04-30 16:44:41 +0800

    dev

---
+++ include/stdio.h
@@ -0,0 +1,15 @@
+#ifndef __STDIO_H
+#define __STDIO_H
+
+/*This is an empty file for the usage of Linkit Smart 7688 U-Boot codes. 
+  The printf function has been defined in the file /common/console.c */ 
+
+#ifdef NEED_STDIO_H
+#include<stdio.h.u-boot>
+#endif /* NEED_STDIO_H */
+
+#if (defined(_WIN32) || defined(_WIN64))/* Windows */
+#include<stdio.h.u-boot>
+#endif/* Windows */
+
+#endif /* __STDIO_H */
